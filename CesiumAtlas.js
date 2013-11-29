571fc33e5d01a1be47a8db6087ca3cc4331bc5ab
Brendan Studds <brendan.studds@gmail.com>
2013-11-29 15:45:19 +1100

    atlas/CesiumAtlas: added function to render a polygon.

--- CesiumAtlas.js
+++ CesiumAtlas.js
@@ -5,9 +5,10 @@ define([
   'atlas/util/Extends',
   'atlas-cesium/dom/DomManager',
   'atlas-cesium/render/RenderManager',
+  'atlas-cesium/model/Polygon',
   // Extends
   'atlas/Atlas'
-], function (extend, DomManager, RenderManager, Atlas) {
+], function (extend, DomManager, RenderManager, Atlas, Polygon) {
 
   var CesiumAtlas = function () {
     CesiumAtlas.base.constructor.call(this);
@@ -20,12 +21,20 @@ define([
     this.managers.dom.setDom(elem, true);
   };
 
-  CesiumAtlas.prototype.addPolygon = function (params) {
-    if (params.id === undefined) {
+  CesiumAtlas.prototype.addPolygon = function (id, vertices, args) {
+    if (id === undefined) {
       throw new DeveloperError('Can not create entity without an ID');
     }
-    var polygon = new Polygong(params.id, params.parent. params.vertices, params.height, params.elevation, params.style, params.material);
+
+    var polygon;
+    if (typeof vertices === 'String') {
+      // Assume vertices is a valid WKT string
+      polygon = Polygon.fromWKT(id, vertices, args);
+    } else {
+      polygon = new Polygon(id, vertices, args);
+    }
     this.managers.render.add(polygon);
+    this.managers.render.show(id);
     return polygon;    
   };
 
