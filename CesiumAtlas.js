ef4eb4270ecdf085002512f33a1e9258cec4010f
Brendan Studds <brendan.studds@gmail.com>
2013-12-05 16:59:12 +1100

    CesiumAtlas: added _managers reference to all Atlas managers, pass _managers to constructors of managers, added function to generate a new feature (may be removed)

--- CesiumAtlas.js
+++ CesiumAtlas.js
@@ -3,22 +3,44 @@
  */
 define([
   'atlas/util/Extends',
+  'atlas/events/EventManager',
   'atlas-cesium/dom/DomManager',
   'atlas-cesium/render/RenderManager',
+  'atlas-cesium/model/Feature',
   'atlas-cesium/model/Polygon',
   // Extends
   'atlas/Atlas'
-], function (extend, DomManager, RenderManager, Polygon, Atlas) {
+], function (extend, EventManager, DomManager, RenderManager, Feature, Polygon, Atlas) {
 
   var CesiumAtlas = function () {
     CesiumAtlas.base.constructor.call(this);
-    this.managers['render'] = new RenderManager();
-    this.managers['dom'] = new DomManager(this.managers.render);
+
+    /* Inherited from Atlas
+    this._managers;
+    */
+    this._managers.events = new EventManager(this._managers);
+    this._managers.render = new RenderManager(this._managers);
+    this._managers.dom = new DomManager(this._managers);
+    console.log(this.addFeature);
   };
   extend(Atlas, CesiumAtlas);
 
-  CesiumAtlas.prototype.attachTo = function (elem) {
-    this.managers.dom.setDom(elem, true);
+  // CesiumAtlas.prototype.attachTo = function (elem) {
+  //   this.managers.dom.setDom(elem, true);
+  // };
+
+  Atlas.prototype.addFeature = function (id, args) {
+    if (typeof id === 'undefined') {
+      throw new DeveloperError('Can add Feature without specifying id');
+    } else {
+      // Add EventManger to the args for the feature.
+      args.eventManager = this._managers.event;
+      // Add the RenderManager to the args for the feature.
+      args.renderManager = this._managers.render;
+      var feature = new Feature(id, args);
+      this._managers.render.addEntity(feature);
+      return feature;
+    }
   };
 
   CesiumAtlas.prototype.addPolygon = function (id, vertices, args) {
@@ -30,8 +52,8 @@ define([
     console.log('poly is visible', polygon.isVisible());
     if (polygon instanceof Polygon) {
       console.log('constructed polygon', polygon);
-      this.managers['render'].addEntity(polygon);
-      this.managers['render'].show(id);
+      this._managers.render.addEntity(polygon);
+      this._managers.render.show(id);
     } else {
       console.log('have', polygon);
       throw 'Error constructing polygon';
