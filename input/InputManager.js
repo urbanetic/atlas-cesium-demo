34021299e09de324e19392a605a0058e46e72aa5
Brendan Studds <brendan.studds@gmail.com>
2013-12-17 16:17:39 +1100

    fixed mouse move handler.

--- input/InputManager.js
+++ input/InputManager.js
@@ -5,21 +5,21 @@ define([
   'atlas-cesium/cesium/Source/Core/ScreenSpaceEventHandler',
   'atlas-cesium/cesium/Source/Core/ScreenSpaceEventType'
 ], function (DeveloperError, defaults, ScreenSpaceEventHandler, ScreenSpaceEventType) {
-  
+
   var InputManager = function (atlasManagers) {
     this._atlasManagers = atlasManagers;
     this._atlasManagers.input = this;
-    
+
     /**
      * Contains a map of Atlas input event names to the Cesium ScreenSpaceEventHandler objects.
      * @type {Object}
      */
     this._handlers = {};
-    
+
     this._element = null;
     this._screenSpaceEventHandler = null;
   }
-  
+
   /**
    * Completes all initialisation that requires other atlas managers.
    * @param {String} domId - The DOM ID of the HTML element to receive events from.
@@ -27,14 +27,23 @@ define([
   InputManager.prototype.initialise = function (domId) {
     this._element = document.getElementById(domId);
     this._screenSpaceEventHandler = new ScreenSpaceEventHandler(this._element);
-    
+
     this.createDefaultBinds();
   }
-  
+
   /**
    * Creates the default bindings between Cesium screen space events and Atlas events.
    */
   InputManager.prototype.createDefaultBinds = function () {
+
+    this._screenSpaceEventHandler.setInputAction(function(movement) {
+      args = {
+        x: movement.position.x,
+        y: movement.position.y
+      };
+      this.handleInternalEvent('input/mousemove', args);
+    }.bind(this._atlasManagers.event), ScreenSpaceEventType.MOUSE_MOVE);
+
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.position.x,
@@ -42,15 +51,7 @@ define([
       };
       this.handleInternalEvent('input/leftdown', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.LEFT_DOWN);
-   
-    this._screenSpaceEventHandler.setInputAction(function(movement) {
-      args = {
-        x: movement.position.x,
-        y: movement.position.y
-      };
-      this.handleInternalEvent('input/leftmove', args);
-    }.bind(this._atlasManagers.event), ScreenSpaceEventType.LEFT_MOVE);
-   
+
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.position.x,
@@ -66,7 +67,7 @@ define([
       };
       this.handleInternalEvent('input/leftclick', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.LEFT_CLICK);
-      
+
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.position.x,
@@ -74,15 +75,7 @@ define([
       };
       this.handleInternalEvent('input/rightdown', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.RIGHT_DOWN);
-   
-    this._screenSpaceEventHandler.setInputAction(function(movement) {
-      args = {
-        x: movement.position.x,
-        y: movement.position.y
-      };
-      this.handleInternalEvent('input/rightmove', args);
-    }.bind(this._atlasManagers.event), ScreenSpaceEventType.RIGHT_MOVE);
-   
+
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.position.x,
@@ -90,7 +83,7 @@ define([
       };
       this.handleInternalEvent('input/rightup', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.RIGHT_UP);
-   
+
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.position.x,
@@ -98,23 +91,24 @@ define([
       };
       this.handleInternalEvent('input/rightclick', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.RIGHT_CLICK);
-   
+
   };
-  
+
   // Add a handler to a particulare input event.
   InputManager.prototype.addHandler = function (name) {};
-  
+
   // Remove a particular input handler
   InputManager.prototype.removeHandler = function (id, name) {};
-  
+
   // Remove all defined handlers
   InputManager.prototype.removeAllHandlers = function () {};
-  
+
   // Ignore events of a particular type?
   InputManager.prototype.ignoreEvent = function (name) {};
-  
+
   // Restore the default handlers defined in the InputManager
   InputManager.prototype.resetDefaultHandlers = function () {};
-  
+
   return InputManager;
 });
+
