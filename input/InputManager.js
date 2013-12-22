098cb8759f1f1fa165514b82f9b0e2ad7600666a
Brendan Studds <brendan.studds@gmail.com>
2013-12-23 00:01:20 +1100

    input/InputManager: added JSDocs. removed _element member. renamed createDefaultBinds to createDefaultBindings. removed unused functions to add new event handlers (pull request #5).

--- input/InputManager.js
+++ input/InputManager.js
@@ -6,43 +6,51 @@ define([
   'atlas-cesium/cesium/Source/Core/ScreenSpaceEventType'
 ], function (DeveloperError, defaults, ScreenSpaceEventHandler, ScreenSpaceEventType) {
 
+  /**
+   * Constructs a new InputManager
+   * @class The InputManager links render and implementation specific user input handling to
+   * the format expected by atlas.
+   * @param {Object} atlasManagers - The map of all atlas manager objects.
+   *
+   * @alias atlas-cesium/input/InputManager
+   * @constructor
+   */
   var InputManager = function (atlasManagers) {
     this._atlasManagers = atlasManagers;
     this._atlasManagers.input = this;
 
     /**
-     * Contains a map of Atlas input event names to the Cesium ScreenSpaceEventHandler objects.
-     * @type {Object}
+     * The Cesium event handlers that are defined.
+     * @type {cesium/Core/ScreenSpaceEventHandler}
+     * @private
      */
-    this._handlers = {};
-
-    this._element = null;
     this._screenSpaceEventHandler = null;
-  }
+  };
 
   /**
    * Completes all initialisation that requires other atlas managers.
    * @param {String|HTMLElement} elem - The DOM ID or DOM element of the HTML element to receive events from.
    */
   InputManager.prototype.initialise = function (elem) {
-    this._element = typeof elem === 'string' ? document.getElementById(elem) : elem;
-    // TODO(bpstudds): Should the InputManager be bound to the Atlas dom element?
-    this._screenSpaceEventHandler = new ScreenSpaceEventHandler(this._element);
-
-    this.createDefaultBinds();
-  }
+    var element = typeof elem === 'string' ? document.getElementById(elem) : elem;
+    // TODO(bpstudds): Pretty sure InputManager should respond to an 'dom/set' event, rather than be
+    //      imperative.
+    this._screenSpaceEventHandler && this._screenSpaceEventHandler.destroy();
+    this._screenSpaceEventHandler = new ScreenSpaceEventHandler(element);
+    this.createDefaultBindings();
+  };
 
   /**
    * Creates the default bindings between Cesium screen space events and Atlas events.
    */
-  InputManager.prototype.createDefaultBinds = function () {
+  InputManager.prototype.createDefaultBindings = function () {
 
     this._screenSpaceEventHandler.setInputAction(function(movement) {
       args = {
         x: movement.endPosition.x,
         y: movement.endPosition.y,
         startX: movement.startPosition.x,
-        startY: movement.startPosition.y,
+        startY: movement.startPosition.y
       };
       this.handleInternalEvent('input/mousemove', args);
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.MOUSE_MOVE);
@@ -96,21 +104,6 @@ define([
     }.bind(this._atlasManagers.event), ScreenSpaceEventType.RIGHT_CLICK);
 
   };
-
-  // Add a handler to a particulare input event.
-  InputManager.prototype.addHandler = function (name) {};
-
-  // Remove a particular input handler
-  InputManager.prototype.removeHandler = function (id, name) {};
-
-  // Remove all defined handlers
-  InputManager.prototype.removeAllHandlers = function () {};
-
-  // Ignore events of a particular type?
-  InputManager.prototype.ignoreEvent = function (name) {};
-
-  // Restore the default handlers defined in the InputManager
-  InputManager.prototype.resetDefaultHandlers = function () {};
 
   return InputManager;
 });
