caae2a9653f01804817da931015c9ba711f522c1
Brendan Studds <brendan.studds@gmail.com>
2013-12-23 00:07:19 +1100

    render/RenderManager: removed some deprecated code regarding event handling of entity/show and adding features (pull request #5).

--- render/RenderManager.js
+++ render/RenderManager.js
@@ -58,29 +58,7 @@ define([
    * Registers event handlers with the EventManager.
    */
   RenderManager.prototype.bindEvents = function () {
-    console.debug('in renderManager', 'binding events');
-    var handlers = [
-      { // Define an event handler for showing an entity.
-        source: 'extern',
-        name: 'entity/show',
-        callback: function (event, args) {
-          // TODO(bpstudds): Move the adding of entities somewhere intelligent.
-          var feature = this._atlasManagers.entity.createFeature(args.id, args);
-          if (feature) feature.show();
-        }.bind(this)
-      },
-      { // Define an event handler for hiding an entity.
-        source: 'extern',
-        name: 'entity/hide',
-        callback: function (event, args) {
-          // TODO(bpstudds): Move the adding of entities somewhere intelligent.
-          var feature = this._atlasManagers.entity.createEntity(args.id)
-          if (feature) feature.hide();
-        }.bind(this)
-      }
-    ];
-    // Add the event handlers to the EventManager.
-    //this._atlasManagers.event.addEventHandlers(handlers);
+    // Nothing to see here. 'entity/show' now handled by CesiumAtlas.
   };
 
   /**
@@ -91,67 +69,17 @@ define([
    */
   RenderManager.prototype.getMinimumTerrainHeight = function (vertices) {
     // TODO(bpstudds): Actually calculate the minimum terrain height.
+    if (vertices || !vertices) {
+      // Using vertices to make IDE warning go away.
+    }
     return 0;
   };
 
-  /**
-   * @deprecated
-   * @see {@link atlas/entity/EntityManager}
-   * Convenience function that creates a new Feature and adds it to the RenderManager.
-   *
-   * @param {Number} id - The ID of this Feature.
-   * @param {Object} args - Parameters describing the feature.
-   * @param {atlas/render/RenderManager} args.renderManager - The RenderManager object responsible for rendering the Feature.
-   * @param {atlas/events/EventManager} args.eventManager - The EventManager object responsible for the event system.
-   * @param {String|Array.atlas/model/Vertex} [args.footprint=null] - Either a WKT string or array of Vertices describing the footprint polygon.
-   * @param {atlas/model/Mesh} [args.mesh=null] - The Mesh object for the Feature.
-   * @param {Number} [args.height=0] - The extruded height when displaying as a extruded polygon.
-   * @param {Number} [args.elevation=0] - The elevation (from the terrain surface) to the base of the Mesh or Polygon.
-   * @param {Boolean} [args.show=false] - Whether the feature should be initially shown when created.
-   * @param {String} [args.displayMode='footprint'] - Initial display mode of feature, one of 'footprint', 'extrusion' or 'mesh'.
-   */
-  RenderManager.prototype.addFeature = function (id, args) {
-    throw new DeveloperError('RenderManager.addFeature is deprecated, use EntityManager.');
-    if (id === undefined) {
-      throw new DeveloperError('Can not add Feature without specifying id');
-    } else {
-      // Add EventManger to the args for the feature.
-      args.eventManager = this._atlasManagers.event;
-      // Add the RenderManager to the args for the feature.
-      args.renderManager = this;
-      var feature = new Feature(id, args);
-      console.debug('cesium-atlas adding feature', args.id, feature);
-      this.addEntity(feature);
-    }
-  };
-
-  /**
-   * Shows a given Entity.
-   * @param {String} id - The ID of the entity to be shown.
-   */
-  RenderManager.prototype.show = function (id) {
-    if (id in this._entities) {
-      console.debug('showing', id);
-      this._entities[id].show();
-    }
-  };
-
-  /**
-   * Hides a given Entity.
-   * @param {String} id - The ID of the entity to be hidden.
-   */
-  RenderManager.prototype.hide = function (id) {
-    if (id in this._entities) {
-      console.debug('hiding', id);
-      this._entities[id].hide();
-    }
-  };
-
   RenderManager.prototype.convertScreenCoordsToLatLng = function (screenCoords) {
-    var cartesian = this._widget.scene.getCamera().controller.pickEllipsoid(screenCoords)
-    var cartographic = this._widget.centralBody.getEllipsoid().cartesianToCartographic(cartesian)
+    var cartesian = this._widget.scene.getCamera().controller.pickEllipsoid(screenCoords);
+    var cartographic = this._widget.centralBody.getEllipsoid().cartesianToCartographic(cartesian);
     var f = 180 / Math.PI;
-    return new Vertex(cartographic.latitude*f, cartographic.longitude*f, cartographic.height);
+    return new Vertex(f * cartographic.latitude, f * cartographic.longitude, cartographic.height);
   };
 
   RenderManager.prototype.getAt = function (screenCoords) {
@@ -161,7 +89,7 @@ define([
       pickedIds.push(p.id);
     });
     return pickedIds;
-  }
+  };
 
   return RenderManager;
 });
