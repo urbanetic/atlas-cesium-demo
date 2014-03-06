b410858c037e5bff64cd2717f76d1be2d075378a
Brendan Studds <brendan.studds@gmail.com>
2014-03-06 13:21:57 +1100

    Bugfix of bugfix/

--- src/model/Polygon.js
+++ src/model/Polygon.js
@@ -125,7 +125,7 @@ define([
         geometry: PolygonGeometry.fromPositions({
           positions: this._cartesians,
           height: elevation,
-          extrudedHeight: elevation + this._showAsExtrusion ? this._height : 0
+          extrudedHeight: elevation + (this._showAsExtrusion ? this._height : 0)
         })
       });
     },
