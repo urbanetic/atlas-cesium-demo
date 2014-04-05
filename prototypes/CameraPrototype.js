afe35f00d34747e7e131587a93a76cb29cb3a8b1
Aram Kocharyan <akarmenia@gmail.com>
2014-04-05 15:02:43 +1100

    Updated render object used in sample. Added test code to switch between bookmarks and convert between positions formats.

--- prototypes/CameraPrototype.js
+++ prototypes/CameraPrototype.js
@@ -1,8 +1,9 @@
 define([
   'utility/Class',
   'atlas/model/Vertex',
-  'atlas/util/AtlasMath'
-], function(Class, Vertex, AtlasMath) {
+  'atlas/util/AtlasMath',
+  'atlas-cesium/cesium/Source/Core/Cartesian3'
+], function(Class, Vertex, AtlasMath, Cartesian3) {
   return Class.extend({
 
     atlas: null,
@@ -11,33 +12,46 @@ define([
       this.atlas = atlas;
 
       var cameraManager = atlas._managers.camera,
-          renderManager = atlas._managers.render;
-      var origCamera = cameraManager._camera;
+          renderManager = atlas._managers.render,
+          camera = cameraManager._camera,
+          cesiumCamera = renderManager.getCesiumCamera();
 
-//            var pos = origCamera.getPosition();
-//            console.error('pos', pos);
-//          origCamera.setPosition();
+      window.printCamera = function() {
+        var position = camera.getPosition();
+        var orientation = camera.getOrientation();
+        console.error(position, orientation);
+      };
+
+      // Converting between units.
+
+//      var cartesian = camera.position;
+//      var cartographic = renderManager.getEllipsoid().cartesianToCartographic(cartesian);
+//      var degrees = new Vertex(AtlasMath.toDegrees(cartographic.latitude),
+//          AtlasMath.toDegrees(cartographic.longitude), cartographic.height);
+//      console.error('pos', cartesian, cartographic, degrees);
 
 //      setInterval(function() {
-//        var camera = renderManager.getCesiumCamera();
-//        var cartesian = camera.position;
-//        var cartographic = renderManager._widget.centralBody.getEllipsoid().cartesianToCartographic(cartesian);
-//        var degrees = new Vertex(AtlasMath.toDegrees(cartographic.latitude),
-//            AtlasMath.toDegrees(cartographic.longitude), cartographic.height);
-//        console.error('pos', cartesian, cartographic, degrees);
+//
+//
 //      }, 3000);
 
-      var posIndex = 0;
-      var positions = [
-        {position: {lat: -37.8136, lng: 144.9631, elevation: 10000}, duration: 3000},
-        {position: {lat: -40.8136, lng: 144.9631, elevation: 8000}, duration: 2000}
-      ];
-      setInterval(function() {
-        posIndex = posIndex % positions.length;
-        var pos = positions[posIndex];
-        origCamera._animateCamera(pos);
-        posIndex++;
-      }, 5000);
+      // Point camera towards a location.
+
+      cesiumCamera.lookUp();
+
+      // Move between bookmarks.
+
+//      var posIndex = 0;
+//      var args = [
+//        {position: {lat: -37.8136, lng: 144.9631, elevation: 10000}, duration: 3000, orientation: {}},
+//        {position: {lat: -40.8136, lng: 144.9631, elevation: 8000}, duration: 2000, orientation: {rotation: -180}}
+//      ];
+//      setInterval(function() {
+//        posIndex = posIndex % args.length;
+//        var arg = args[posIndex];
+//        origCamera._animateCamera(arg);
+//        posIndex++;
+//      }, 5000);
 
     }
 
