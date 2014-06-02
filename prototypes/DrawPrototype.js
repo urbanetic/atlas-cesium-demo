25dc3d01be60ada0b610adf73eab169ec144e8c1
Aram Kocharyan <akarmenia@gmail.com>
2014-06-03 09:59:09 +1000

    Some more changes for drawing. No longer necessary but committing them to this branch.

--- prototypes/DrawPrototype.js
+++ prototypes/DrawPrototype.js
@@ -1,6 +1,7 @@
 define([
-  'atlas/util/Class'
-], function(Class) {
+  'atlas/util/Class',
+  'jquery'
+], function(Class, $) {
   return Class.extend({
 
     atlas: null,
@@ -8,24 +9,32 @@ define([
     _init: function(atlas) {
       this.atlas = atlas;
 
-      var count = 0;
+      $(function() {
+        $('#cesium').css({
+          'padding-left': '100px',
+          'padding-top': '100px'
+        });
+      });
+
+//      var count = 0;
       var drawMode = function() {
         console.error('entity/draw', arguments);
         atlas.publish('entity/draw', {
           update: function() {
             console.error('update', arguments);
-            count++;
-            if (count === 5) {
-              atlas.publish('entity/draw/stop');
-              count = 0;
-            }
+//            count++;
+//            if (count === 5) {
+//              atlas.publish('entity/draw/stop');
+//              count = 0;
+//            }
           },
           create: function() {
             console.error('create', arguments);
             setTimeout(drawMode, 3000);
           },
-          cancel: function (args) {
+          cancel: function(args) {
             console.error('cancel', arguments);
+            setTimeout(drawMode, 3000);
           }
         });
       };
