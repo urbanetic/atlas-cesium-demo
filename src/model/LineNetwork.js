135fcf25826303e4e20ae5f5598d7e5012cec161
Brendan Studds <brendan.studds@gmail.com>
2014-07-05 09:37:27 +1000

    Added basic atlas-cesium LineNetwork implementation.

---
+++ src/model/LineNetwork.js
@@ -0,0 +1,31 @@
+define([
+  'atlas-cesium/model/Line',
+  // Base class
+  'atlas/model/LineNetwork'
+], function (Line, LineNetworkCore) {
+  /**
+   * @typedef atlas-cesium.model.LineNetwork
+   * @ignore
+   */
+  var LineNetwork;
+
+  /**
+   * The Atlas-Cesium implementation of @link{atlas.model.LineNetwork}.
+   * @class atlas-cesium.model.LineNetwork
+   * @extends atlas.model.LineNetwork
+   */
+  LineNetwork = LineNetworkCore.extend({
+    /**
+     * The array of Lines constructing the line network.
+     * @type {atlas-cesium.model.Line}
+     * @private
+     */
+    _lines: null,
+
+    _createLine: function (id, lineData, args) {
+      return new Line(id, lineData, args);
+    }
+  });
+
+  return LineNetwork;
+});
