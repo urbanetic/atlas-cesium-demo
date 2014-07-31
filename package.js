fbc3c75c1ffb69e004e9a2c389ece2a6e1dd59c3
Aram Kocharyan <akarmenia@gmail.com>
2014-07-31 23:19:47 +1000

    More docs.

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
+// Meteor package definition.
 Package.describe({
   summary: 'An AGI Cesium <cesiumjs.org> implementation of the Atlas rendering API.'
 });
 
 Package.on_use(function (api) {
+  // Depends on the requirejs package.
   api.use('requirejs', 'client');
+  // Make the built source code and styles available for the client.
   api.add_files(['dist/atlas-cesium.min.js', 'dist/atlas-cesium.min.css'], 'client');
 });
