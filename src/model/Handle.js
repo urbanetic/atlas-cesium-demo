da61e1198880b4b54dbd3422a227af9300d8884f
Aram Kocharyan <akarmenia@gmail.com>
2014-08-07 23:35:26 +1000

    Initial implementation of handles as billboards. Still can't see the image though.

--- src/model/Handle.js
+++ src/model/Handle.js
@@ -1,9 +1,12 @@
 define([
   'atlas/model/GeoPoint',
   'atlas-cesium/model/Ellipse',
+  'atlas-cesium/cesium/Source/Scene/BillboardCollection',
+  'atlas-cesium/cesium/Source/Core/Cartesian2',
+  'atlas-cesium/cesium/Source/Core/Cartesian3',
   // Base class.
   'atlas/model/Handle'
-], function(GeoPoint, Ellipse, HandleCore) {
+], function(GeoPoint, Ellipse, BillboardCollection, Cartesian2, Cartesian3, HandleCore) {
 
   /**
    * @typedef atlas-cesium.model.Handle
@@ -20,35 +23,40 @@ define([
    */
   Handle = HandleCore.extend(/** @lends atlas.model.Handle# */ {
 
-    /**
-     * The Ellipse representing the handle.
-     * @type {atlas-cesium.model.Ellipse}
-     * @private
-     */
+    // TODO(aramk) Store billboard collection on the owner.
+
     _dot: null,
 
-    /**
-     * Creates a new dot instance
-     * @param args
-     * @returns {Ellipse}
-     * @private
-     */
     _createDot: function(args) {
-      return new Ellipse(this.getId(),
-          {centroid: this._target, semiMajor: this._dotRadius}, args);
+      //return new Ellipse(this.getId(), {centroid: this._target, semiMajor: this._dotRadius}, args);
+      var renderManager = this._renderManager;
+      var primitives = renderManager.getPrimitives();
+      var billboards = primitives.add(new BillboardCollection());
+      var position = renderManager.cartesianFromGeoPoint(this._target);
+      var image = new Image(10, 10);
+      image.src = 'http://localhost/urbanetic/atlas-cesium-demo/atlas/resources/images/handle.png';
+      billboards.add({
+        image: image,
+        height: 10,
+        width: 10,
+        show: true,
+        pixelOffset: new Cartesian2(0.0, 1.0),
+        eyeOffset: new Cartesian3(0, 0, 100),
+        position: position
+      });
     },
 
     show: function() {
-      return this._dot.show();
+      //return this._dot.show();
     },
 
     hide: function() {
-      return this._dot.hide();
+      //return this._dot.hide();
     },
 
     remove: function() {
       this._super();
-      this._dot.remove();
+      //this._dot.remove();
     }
 
   });
