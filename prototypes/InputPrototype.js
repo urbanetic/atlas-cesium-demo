e1b1e3507302418c268173287a43057df9155a91
Aram Kocharyan <akarmenia@gmail.com>
2014-08-11 18:14:15 +1000

    Added new input prototype for checking the location of mouse click events. Added build.html for testing the built packages.

---
+++ prototypes/InputPrototype.js
@@ -0,0 +1,23 @@
+define([
+  'atlas/lib/utility/Class',
+  'jquery'
+], function(Class, $) {
+  return Class.extend({
+
+    atlas: null,
+
+    _init: function(atlas) {
+      this.atlas = atlas;
+      var renderManager = atlas._managers.render;
+      window.toRadians = function(a) {
+        return a * 180 / Math.PI;
+      };
+      atlas.subscribe('input/leftup', function(args) {
+        console.log('screen', args.position);
+        var point = renderManager.geoPointFromScreenCoords(args.position);
+        console.log('point', point);
+      });
+    }
+
+  });
+});
