e09cae4fdf14107a197e9a57aee129b2d2262a06
Aram Kocharyan <akarmenia@gmail.com>
2014-08-20 12:57:32 +1000

    Changed path for c3ml mesh.

--- prototypes/MeshPrototype.js
+++ prototypes/MeshPrototype.js
@@ -10,10 +10,11 @@ define([
     _init: function(atlas) {
       this.atlas = atlas;
       var entityManager = atlas._managers.entity;
-      $.getJSON('assets/VIC_SH_2St_3Bed.c3ml.json', function (c3ml) {
+      $.getJSON('assets/VIC_SH_2St_3Bed_roof.c3ml.json', function(c3ml) {
         console.log('c3ml', c3ml);
         atlas.publish('entity/show/bulk', {features: [c3ml]});
-        atlas.publish('camera/zoomTo', {position: c3ml.geoLocation});
+        var loc = c3ml.geoLocation;
+        atlas.publish('camera/zoomTo', {position: {longitude: loc[0], latitude: loc[1], elevation: 100}});
       });
     }
 
