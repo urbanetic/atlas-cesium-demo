420ec064b891b43d6eb1988b7cd2486b0ec5109e
Aram Kocharyan <akarmenia@gmail.com>
2014-08-26 23:15:56 +1000

    Updated to latest develop branches.

--- prototypes/MeshPrototype.js
+++ prototypes/MeshPrototype.js
@@ -1,8 +1,10 @@
 define([
   'atlas/lib/utility/Class',
   'atlas/model/GeoEntity',
+  'atlas/model/GeoPoint',
+  'atlas-cesium/model/Handle',
   'jquery'
-], function(Class, GeoEntity, $) {
+], function(Class, GeoEntity, GeoPoint, Handle, $) {
   return Class.extend({
 
     atlas: null,
@@ -12,9 +14,18 @@ define([
       var entityManager = atlas._managers.entity;
       $.getJSON('assets/VIC_SH_2St_3Bed_roof.c3ml.json', function(c3ml) {
         console.log('c3ml', c3ml);
+        c3ml.geoLocation = [145.253159238, -37.81175024725, 0];
         atlas.publish('entity/show/bulk', {features: [c3ml]});
-        var loc = c3ml.geoLocation;
-        atlas.publish('camera/zoomTo', {position: {longitude: loc[0], latitude: loc[1], elevation: 100}});
+        var point = new GeoPoint(c3ml.geoLocation);
+        atlas.publish('camera/zoomTo', {position: point});
+        // TODO(aramk) Create handle on mesh.
+        var id = c3ml.id;
+        var mesh = entityManager.getById(id);
+        new Handle(mesh._bindDependencies({target: point, owner: mesh}));
+        setTimeout(function () {
+          console.log('translate');
+          mesh.translate(new GeoPoint({latitude: 0.001, longitude: 0.001}));
+        }, 3000);
       });
     }
 
