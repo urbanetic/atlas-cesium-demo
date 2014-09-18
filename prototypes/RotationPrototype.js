bb783fa6c5a8623e6b46bf2d879e40c2f14eb44a
Aram Kocharyan <akarmenia@gmail.com>
2014-09-19 09:42:55 +1000

    Added example of rotation and scaling.

--- prototypes/RotationPrototype.js
+++ prototypes/RotationPrototype.js
@@ -1,7 +1,8 @@
 define([
   'atlas/lib/utility/Class',
+  'atlas/model/GeoPoint',
   'atlas/model/Vertex'
-], function(Class, Vertex) {
+], function(Class, GeoPoint, Vertex) {
   return Class.extend({
 
     atlas: null,
@@ -11,10 +12,21 @@ define([
 //      atlas.publish('sleepMode', false);
       var entityManager = atlas._managers.entity;
       var features = entityManager.getFeatures();
-      var feature = features[0];
-      setInterval(function() {
-        feature.rotate(new Vertex(0, 0, 90));
-      }, 1000);
+//      var feature = features[0];
+      features.forEach(function(feature) {
+        feature.translate(new GeoPoint(0.003, 0.003));
+        feature.setRotation(new Vertex(0, 0, 90));
+        feature.setScale(new Vertex(1.5, 1.5, 4));
+        setTimeout(function () {
+          feature.setDisplayMode('footprint');
+        }, 5000);
+        setTimeout(function () {
+          feature.setDisplayMode('extrusion');
+        }, 8000);
+      });
+//      setInterval(function() {
+//        feature.rotate(new Vertex(0, 0, 90));
+//      }, 1000);
     }
 
   });
