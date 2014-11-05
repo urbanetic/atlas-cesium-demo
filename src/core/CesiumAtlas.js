32c6bca3ddad4679bb123af5caf2b4f826b36c1e
Aram Kocharyan <akarmenia@gmail.com>
2014-11-06 02:01:47 +1100

    Refactored setup and common atlas managers to atlas.

--- src/core/CesiumAtlas.js
+++ src/core/CesiumAtlas.js
@@ -1,16 +1,11 @@
 define([
   'atlas/core/Atlas',
-  'atlas/edit/EditManager',
-  'atlas/events/EventManager',
-  'atlas/selection/SelectionManager',
-  'atlas/visualisation/VisualisationManager',
   'atlas-cesium/camera/CameraManager',
   'atlas-cesium/dom/DomManager',
   'atlas-cesium/entity/EntityManager',
   'atlas-cesium/input/InputManager',
   'atlas-cesium/render/RenderManager'
-], function(Atlas, EditManager, EventManager, SelectionManager, VisualisationManager, CameraManager,
-            DomManager, EntityManager, InputManager, RenderManager) {
+], function(Atlas, CameraManager, DomManager, EntityManager, InputManager, RenderManager) {
 
   /**
    * @typedef atlas.core.CesiumAtlas
@@ -27,26 +22,10 @@ define([
     _init: function() {
       this._super();
 
-      // Create all the atlas manager objects before initialising any. Any initialisation work
-      // that requires the presence of a particular manager is done in <code>setup()</code>,
-      // so the managers may be created in any order.
-      this.setManager(new EditManager(this._managers));
-      this.setManager(new EntityManager(this._managers));
-      this.setManager(new EventManager(this._managers));
       this.setManager(new RenderManager(this._managers));
       this.setManager(new DomManager(this._managers));
-      this.setManager(new SelectionManager(this._managers));
       this.setManager(new InputManager(this._managers));
       this.setManager(new CameraManager(this._managers));
-      this.setManager(new VisualisationManager(this._managers));
-
-      // Setup the manager objects. These are independent unless stated otherwise.
-      this._managers.camera.setup();
-      this._managers.edit.setup();
-      this._managers.render.setup();
-      this._managers.entity.setup();
-      this._managers.selection.setup();
-      this._managers.visualisation.setup();
 
       /**
        * Contains a map of event name to EventHandler object.
@@ -54,6 +33,13 @@ define([
        * @private
        */
       this._eventHandlers = {};
+    },
+
+    _setup: function() {
+      this._super();
+      this._managers.camera.setup();
+      this._managers.edit.setup();
+      this._managers.render.setup();
     },
 
     /**
