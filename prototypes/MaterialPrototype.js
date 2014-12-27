a865289c85666fb500189fbded244dd6cd1d1629
Aram Kocharyan <akarmenia@gmail.com>
2014-12-28 00:14:25 +1100

    Added mesh prototype example showing how to render a polygon with materials using c3ml arguments rather than object references.

--- prototypes/MaterialPrototype.js
+++ prototypes/MaterialPrototype.js
@@ -2,8 +2,9 @@ define([
   'atlas/lib/utility/Class',
   'atlas/material/CheckPattern',
   'atlas/material/Color',
-  'atlas/material/Style'
-], function(Class, CheckPattern, Color, Style) {
+  'atlas/material/Style',
+  'atlas/model/GeoPoint'
+], function(Class, CheckPattern, Color, Style, GeoPoint) {
   return Class.extend({
 
     atlas: null,
@@ -12,15 +13,44 @@ define([
       this.atlas = atlas;
       
       var features = atlas._managers.entity.getFeatures();
-      var someFeature = features[0];
-      console.log('someFeature', someFeature);
+      var featureA = features[0];
+      var featureB = features[1];
+
+      // Create material programmatically.
 
       var checkPattern = new CheckPattern({lightColor: Color.YELLOW, darkColor: Color.RED});
-      var currentStyle = someFeature.getStyle();
+      var currentStyle = featureA.getStyle();
       var style = new Style({fillMaterial: checkPattern,
           borderMaterial: currentStyle.getBorderMaterial()});
-      someFeature.setStyle(style);
-      
+      featureA.setStyle(style);
+
+      var translation = new GeoPoint(0.001, 0.001);
+      var positions = featureA.getVertices().map(function(point) {
+        return point.translate(translation);
+      });
+
+      // Create material with c3ml.
+
+      var featureBId = 'material-polygon';
+      atlas.publish('entity/show', {
+        id: featureBId,
+        polygon: {
+          vertices: positions,
+          height: 50,
+          fillMaterial: {
+            type: 'CheckPattern',
+            lightColor: 'green',
+            darkColor: 'red'
+          },
+          borderMaterial: {
+            type: 'Color',
+            blue: 1
+          }
+        }
+      });
+
+      var featureB = atlas._managers.entity.getById(featureBId);
+        
     }
 
   });
