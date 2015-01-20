9a75fb09d04d6021e60ecfc2260637a83bb9b4f0
Aram Kocharyan <akarmenia@gmail.com>
2015-01-21 10:38:26 +1100

    Added support for line widths on polygons.

--- src/model/Polygon.js
+++ src/model/Polygon.js
@@ -150,9 +150,9 @@ define([
       if (borderMaterial) {
         if ((isModelDirty || !this._outlinePrimitive) && this._outlineGeometry) {
           // TODO(aramk) This causes an issue on high-res screens.
-          // var lineWidth = style.getBorderWidth();
-          // lineWidth = Setter.range(lineWidth, 2, scene.maximumAliasedLineWidth);
-          var lineWidth = Math.min(2, scene.maximumAliasedLineWidth);
+          var lineWidth = Setter.range(style.getBorderWidth(), 2, scene.maximumAliasedLineWidth);
+          console.log('lineWidth', style.getBorderWidth(), 2, scene.maximumAliasedLineWidth);
+          // var lineWidth = Math.min(2, scene.maximumAliasedLineWidth);
           this._outlinePrimitive = new Primitive({
             geometryInstances: this._outlineGeometry,
             // TODO(aramk) https://github.com/AnalyticalGraphicsInc/cesium/issues/2052
