2e632ab1cbd53e841f6a15c3d0b9af01738fd3f0
Aram Kocharyan <akarmenia@gmail.com>
2015-01-22 12:54:47 +1100

    Cleanup.

--- src/model/Polygon.js
+++ src/model/Polygon.js
@@ -149,12 +149,9 @@ define([
       }
       if (borderMaterial) {
         if ((isModelDirty || !this._outlinePrimitive) && this._outlineGeometry) {
-          // TODO(aramk) This causes an issue on high-res screens.
           var maxWidth = scene.maximumAliasedLineWidth;
           var minWidth = Math.min(2, maxWidth);
           var lineWidth = Setter.range(style.getBorderWidth(), minWidth, maxWidth);
-          console.log('lineWidth', style.getBorderWidth(), 2, scene.maximumAliasedLineWidth);
-          // var lineWidth = Math.min(2, scene.maximumAliasedLineWidth);
           this._outlinePrimitive = new Primitive({
             geometryInstances: this._outlineGeometry,
             // TODO(aramk) https://github.com/AnalyticalGraphicsInc/cesium/issues/2052
