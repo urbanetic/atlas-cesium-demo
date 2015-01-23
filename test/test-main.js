ddf3484ba601d1f6f83e52422c604b9b1d0c9c1a
Brendan Studds <brendan.studds@gmail.com>
2015-01-23 16:47:03 +1100

    Style and formatting fixes

--- test/test-main.js
+++ test/test-main.js
@@ -1,9 +1,9 @@
 var tests = [];
 var specsConfig = [
-  { name: 'camera/Camera', run: true },
-  { name: 'model/Ellipse', run: true },
-  { name: 'model/Handle', run: true },
-  { name: 'model/Mesh', run: true }
+  {name: 'camera/Camera', run: true},
+  {name: 'model/Ellipse', run: true},
+  {name: 'model/Handle', run: true},
+  {name: 'model/Mesh', run: true}
 ];
 
 var warnings = '\n';
@@ -19,29 +19,20 @@ specsConfig.forEach(function(config) {
 /* global console */
 warnings !== '\n' && console.log(warnings);
 
-//for (var file in window.__karma__.files) {
-//  if (window.__karma__.files.hasOwnProperty(file)) {
-//    if (/.*Spec\.js$/.test(file)) {
-//      console.debug('test spec:', file);
-//      tests.push(file);
-//    }
-//  }
-//}
-
 /* global requirejs,window */
 requirejs.config({
   // Karma serves files from '/base'.
   baseUrl: '/base',
 
   packages: [
-    { name: 'jquery', location: 'atlas/lib', main: 'jquery.js'},
-    { name: 'atlas/lib', location: 'atlas/lib'},
-    { name: 'atlas/lib/utility', location: 'atlas/lib/utility/src'},
-    { name: 'atlas/assets', location: 'atlas/assets'}, // Only need this for testing
-    { name: 'atlas', location: 'atlas/src'},
-    { name: 'atlas-cesium/cesium', location: 'atlas-cesium/lib/cesium'},
-    { name: 'atlas-cesium/lib', location: 'atlas-cesium/lib'},
-    { name: 'atlas-cesium', location: 'atlas-cesium/src'}
+    {name: 'jquery', location: 'atlas/lib', main: 'jquery.js'},
+    {name: 'atlas/lib', location: 'atlas/lib'},
+    {name: 'atlas/lib/utility', location: 'atlas/lib/utility/src'},
+    {name: 'atlas/assets', location: 'atlas/assets'}, // Only need this for testing
+    {name: 'atlas', location: 'atlas/src'},
+    {name: 'atlas-cesium/cesium', location: 'atlas-cesium/lib/cesium'},
+    {name: 'atlas-cesium/lib', location: 'atlas-cesium/lib'},
+    {name: 'atlas-cesium', location: 'atlas-cesium/src'}
   ],
 
   // Ask requirejs to load these files.
