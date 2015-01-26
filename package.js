dacb20357247f92b48c0cbd1d9034c0867ad1e8b
Aram Kocharyan <akarmenia@gmail.com>
2015-01-26 11:01:29 +1100

    Version bump for meteor.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 // Meteor package definition.
 Package.describe({
   name: 'urbanetic:atlas-cesium',
-  version: '0.7.0-beta',
+  version: '0.7.0_1',
   summary: 'An AGI Cesium <cesiumjs.org> implementation of the Atlas rendering API.',
   git: 'https://bitbucket.org/mutopia/atlas-cesium.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use('aramk:requirejs@2.1.15', ['client', 'server']);
+  api.use('aramk:requirejs@2.1.15_1', ['client', 'server']);
   api.addFiles(['dist/atlas-cesium.min.js'], ['client', 'server']);
   api.addFiles(['dist/resources/atlas-cesium.min.css'], 'client');
 });
