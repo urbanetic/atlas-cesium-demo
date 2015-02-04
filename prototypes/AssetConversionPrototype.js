d0af2fbdc837f96cb37f603dc6a4dc265ee1c4a8
Aram Kocharyan <akarmenia@gmail.com>
2015-02-04 17:34:30 +1100

    Added dropzone prototype and style for asset conversion.

---
+++ prototypes/AssetConversionPrototype.js
@@ -0,0 +1,37 @@
+define([
+  'atlas/lib/utility/Class',
+  'atlas/material/Color',
+  'atlas/material/Style',
+  'atlas/model/GeoPoint',
+  'atlas/model/Point',
+  'atlas/util/WKT',
+  'jquery',
+  'dropzone'
+], function(Class, Color, Style, GeoPoint, Point, WKT, $, Dropzone) {
+  return Class.extend({
+
+    atlas: null,
+
+    _init: function(atlas) {
+      this.atlas = atlas;
+      console.log('Starting ACS');
+      var $dropzone = $('<div class="dropzone"></div>');
+      $('body').append($dropzone);
+      var dropzone = new Dropzone($dropzone[0], {
+        url: '/assets/upload',
+        dictDefaultMessage: 'Drop a file here or click to upload.',
+        addRemoveLinks: false
+      });
+      dropzone.on('sending', function() {
+        console.log('sending', arguments);
+      });
+      dropzone.on('error', function() {
+        console.log('error', arguments);
+      });
+      dropzone.on('success', function() {
+        console.log('success', arguments);
+      });
+    }
+
+  });
+});
