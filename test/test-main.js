48abd8ffdc87fa148ac8bfec4ec0c16086ae61ce
Aram Kocharyan <akarmenia@gmail.com>
2015-02-13 00:01:41 +1100

    Added subdiv to tests.

--- test/test-main.js
+++ test/test-main.js
@@ -26,14 +26,26 @@ requirejs.config({
   baseUrl: '/base',
 
   packages: [
-    {name: 'jquery', location: 'atlas/lib', main: 'jquery.js'},
+    {name: 'atlas', location: 'atlas/src'},
     {name: 'atlas/lib', location: 'atlas/lib'},
     {name: 'atlas/lib/utility', location: 'atlas/lib/utility/src'},
     {name: 'atlas/assets', location: 'atlas/assets'}, // Only need this for testing
-    {name: 'atlas', location: 'atlas/src'},
     {name: 'atlas-cesium/cesium', location: 'atlas-cesium/lib/cesium'},
     {name: 'atlas-cesium/lib', location: 'atlas-cesium/lib'},
-    {name: 'atlas-cesium', location: 'atlas-cesium/src'}
+    {name: 'atlas-cesium', location: 'atlas-cesium/src'},
+    {name: 'jquery', location: 'atlas/lib', main: 'jquery.js'},
+
+    // These all belong to subdiv.
+    {name: 'atlas/lib/subdiv', location: 'atlas/lib/subdiv/src'},
+    {name: 'ConvexHullGrahamScan', location: 'atlas/lib/subdiv/lib', main: 'ConvexHullGrahamScan.js'},
+    {name: 'hull', location: 'atlas/lib/subdiv/lib', main: 'hull.js'},
+    {name: 'jsts', location: 'atlas/lib/subdiv/lib/jsts'},
+    {name: 'tinycolor', location: 'atlas/lib/subdiv/lib', main: 'tinycolor.js'},
+    {name: 'underscore', location: 'atlas/lib/subdiv/lib', main: 'underscore.js'},
+    {name: 'utility', location: 'atlas/lib/subdiv/lib/utility'},
+
+    // This is the expected name of utm-converter in subdiv.
+    {name: 'utm-converter', location: 'atlas/lib', main: 'UtmConverter.js'}
   ],
 
   // Ask requirejs to load these files.
