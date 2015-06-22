49b18217cd0f1b56d1501f8117adceebb855890e
Aram Kocharyan <akarmenia@gmail.com>
2015-06-22 21:59:55 +1000

    Fixed bugs with projections and selections.

--- prototypes/ProjectionPrototype.js
+++ prototypes/ProjectionPrototype.js
@@ -18,10 +18,14 @@ define([
       // Generate dummy values for projection.
       var values = {};
       features.forEach(function(feature, i) {
+        // NOTE: Should not get different results whether using features or their forms.
         var id = feature.getId();
+        // var id = feature.getForm().getId();
         values[id] = i;
         // feature.setHeight(0);
       });
+
+      features[0].setSelected(true);
 
       // var opacity = 0.6;
       // var red = Color.RED.clone();
@@ -29,41 +33,43 @@ define([
       // var green = Color.GREEN.clone();
       // green.alpha = opacity;
 
-      var args = {
-        type: 'color',
-        ids: Object.keys(values),
-        config: {
-          title: 'Color Projection',
-          values: values,
-          opacity: 0.6,
-          codomain: {
-            startProj: '#00ff00',
-            endProj: '#ff0000'
-          }
-        }
-      };
-      atlas.publish('projection/add', args);
-      atlas.publish('projection/render', {id: args.projection.getId()});
-
-      var args2 = {
-        type: 'height',
-        ids: Object.keys(values),
-        config: {
-          title: 'Height Projection',
-          values: values,
-          codomain: {
-            startProj: 5,
-            endProj: 300
-          }
-        }
-      };
-      atlas.publish('projection/add', args2);
-      atlas.publish('projection/render', {id: args2.projection.getId()});
-
       setTimeout(function() {
-        atlas.publish('projection/remove', {id: args.projection.getId()});
-        atlas.publish('projection/remove', {id: args2.projection.getId()});
-      }, 5000);
+        var args = {
+          type: 'color',
+          ids: Object.keys(values),
+          config: {
+            title: 'Color Projection',
+            values: values,
+            opacity: 0.6,
+            codomain: {
+              startProj: '#00ff00',
+              endProj: '#ff0000'
+            }
+          }
+        };
+        atlas.publish('projection/add', args);
+        atlas.publish('projection/render', {id: args.projection.getId()});
+
+        var args2 = {
+          type: 'height',
+          ids: Object.keys(values),
+          config: {
+            title: 'Height Projection',
+            values: values,
+            codomain: {
+              startProj: 5,
+              endProj: 300
+            }
+          }
+        };
+        atlas.publish('projection/add', args2);
+        atlas.publish('projection/render', {id: args2.projection.getId()});
+
+        setTimeout(function() {
+          atlas.publish('projection/remove', {id: args.projection.getId()});
+          atlas.publish('projection/remove', {id: args2.projection.getId()});
+        }, 5000);
+      }, 2000);
     }
 
   });
